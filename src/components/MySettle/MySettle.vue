f2efb815f0be9f5dc4c91511ef651b3b21d6025e
i hu <ihuwe@ihu>
2023-08-15 18:15:36 +0800

    完成

--- src/components/MySettle/MySettle.vue
+++ src/components/MySettle/MySettle.vue
@@ -23,14 +23,17 @@ import { mapState, mapMutations, mapGetters } from 'vuex'
 export default {
   name: "MySettle",
   data() {
-    return {};
+    return {
+      n: 3,
+      timer: null
+    };
   },
   computed: {
-    ...mapState(['cart','aHeight']),
-    ...mapGetters(['sumPrice', 'selectAll', 'checkedCount'])
+    ...mapState(['cart', 'token']),
+    ...mapGetters(['addstr', 'sumPrice', 'selectAll', 'checkedCount'])
   },
   methods: {
-    ...mapMutations(['cancelState']),
+    ...mapMutations(['cancelState', 'updatecart', 'delcart']),
     cancelAll() {
       if (this.selectAll) {
         this.cancelState(true)
@@ -39,7 +42,66 @@ export default {
       }
     },
     balance() {
-      console.log(this.sumPrice, !this.selectAll, this.checkedCount);
+      // 先判断是否勾选了要结算的商品
+      if (this.checkedCount === 0) return uni.$showMsg('请选择要结算的商品！')
+      // 再判断用户是否选择了收货地址
+      if (!this.addstr) return uni.$showMsg('请选择收货地址！')
+      // 最后判断用户是否登录了
+      if (!this.token) {
+        uni.$showMsg(this.n + ' 秒后跳转登录页', 1000, 'loading')
+        this.timer = setInterval(() => {
+          this.n--
+          uni.$showMsg(this.n + ' 秒后跳转登录页', 2000, 'loading')
+          if (this.n <= 0) {
+            clearInterval(this.timer)
+            return uni.switchTab({ url: '/pages/my/my' })
+          }
+        }, 1000)
+        return
+      }
+      // 实现微信支付功能
+      this.payOrder()
+    },
+    // 微信支付
+    async payOrder() {
+      // 创建订单,发起请求创建订单
+      // const { data: res } = await uni.$http.post('/api/public/v1/my/orders/create', {
+      //   // 开发期间，注释掉真实的订单价格，
+      //   order_price: this.sumPrice,
+      //   consignee_addr: this.addstr,
+      //   goods: this.cart.filter(x => x.goods_state).map(x => ({ goods_id: x.goods_id, goods_number: x.goods_count, goods_price: x.goods_price }))
+      // })
+      // if (res.meta.status !== 200) return uni.$showMsg('创建订单失败！')
+      // 得到服务器响应的“订单编号”,发起请求获取订单的支付信息
+      // const { data: res2 } = await uni.$http.post('/api/public/v1/my/orders/req_unifiedorder', { order_number: res.message.order_number })
+      // 预付订单生成失败
+      // if (res2.meta.status !== 200) return uni.$showError('预付订单生成失败！')
+      // 得到订单支付相关的必要参数,调用 uni.requestPayment() 发起微信支付
+
+      // const pay = await uni.requestPayment(res2.message.pay)
+      // 未完成支付
+      // if (err) return uni.$showMsg('订单未支付！')
+      // 完成了支付，进一步查询支付的结果
+      // const { data: res3 } = await uni.$http.post('/api/public/v1/my/orders/chkOrder', { order_number: orderNumber })
+      // 检测到订单未支付
+      // if (res3.meta.status !== 200) return uni.$showMsg('订单未支付！')
+      // 检测到订单支付完成
+      if (!this.token) {
+        uni.$showMsg('订单支付失败', 1500, 'error');
+        uni.showModal({
+          title: '提示',
+          content: '是否清除购物车商品？',
+          success: (res) => {
+            if (res.confirm) {
+              this.delcart()
+            }
+          }
+        })
+        return
+      }
+      uni.$showMsg('支付完成', 1500, 'success');
+      this.updatecart()
+      return uni.navigateTo({ url: '/subpkg/orderForm/orderForm' })
     }
   }
 }
---
+++ src/subpkg/orderForm/orderForm.vue
@@ -0,0 +1,87 @@
+<template>
+  <view class="order">
+    <view v-for="(value, key) in cartList" :key="key" class="name">
+      <view class="price">
+        <text class="text">订单号：</text><text style="color: #95a5a6;">{{ key }}</text>
+      </view>
+      <view class="goods">
+        <view v-for="(item, i) in value" :key="i" class="goods-info">
+          <image :src="item.goods_small_logo" mode="scaleToFill" />
+          <text style="font-size: 20rpx;color: red;">数量：{{ item.goods_count }}</text>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+import { mapState, mapMutations, mapGetters } from 'vuex'
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+    ...mapState(['cartList']),
+  },
+  onLoad() {
+    console.log(this.cartList);
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.order {
+  .name {
+    // display: flex;
+    // 
+    // justify-content: space-around;
+    // align-items: center;
+    padding: 10rpx 20rpx;
+
+    .price {
+      display: flex;
+      align-items: center;
+      overflow: hidden;
+      font-size: 25rpx;
+      color: #b2bec3;
+      margin-bottom: 10rpx;
+      .text{
+        display: block;
+        width: 130rpx;
+      }
+    }
+
+    .goods {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: start;
+      align-items: center;
+      padding: 15rpx 10rpx;
+      background-color: #f1f2f6;
+      box-shadow: 0 5rpx 15rpx 5rpx rgb(206, 214, 224);
+
+      .goods-info {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        width: 230rpx;
+
+        image {
+          width: 230rpx;
+          height: 200rpx;
+          margin-bottom: 5rpx;
+        }
+      }
+    }
+  }
+}
+</style>
+    
+      
